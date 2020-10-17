ad5c00a7faa51e9097797a74c21bec2590be2010
jhylands <hylands.james@gmail.com>
2020-10-17 17:58:12 +0000

    Initial commit, some definitions
    Mocking out some definitions

---
+++ definitions.py
@@ -0,0 +1,31 @@
+class Function:
+    def __init__(self):
+        self.commutitive
+        self.asociative
+
+    def __call__(self, expression):
+        pass
+
+class Term:
+    def __init__(self):
+        self.tree
+    
+class Expression:
+    def __init__(self):
+        self.terms = []
+
+    def __iter__(self):
+        return iter(self.terms)
+
+class Equation:
+    def __init__(self, lhs, rhs):
+        # type: (Equation, Expression, Expression)->None
+        self.lhs = lhs
+        self.rhs = rhs
+
+    def apply(self, f):
+        # Maybe we need to work out how best to define f
+        self.lhs = f(self.lhs)
+        self.rhs = f(self.rhs)
+
+
